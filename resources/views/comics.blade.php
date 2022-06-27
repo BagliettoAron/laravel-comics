3cc5cf5fe3458985deb06e3c9ca4e4b300d3d256
@BagliettoAron <aronbaglietto88@gmail.com>
2022-06-27 15:48:44 +0200

    WIP: scaffolding

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title')
+    Comics
+@endsection
+
+@section('main_content')
+sono un fumetto singolo
+    
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title')
+title test
+    
+@endsection
+
+@section('main_content')
+    test
+@endsection
