0f7f2c87b01c37953527cde7969e9a3440f8691a
@BagliettoAron <aronbaglietto88@gmail.com>
2022-06-27 18:42:02 +0200

    WIP: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,42 @@
 <footer>
-    footer
-  </footer>
+  <div class="topFooterBg">
+    <div class="container">
+
+      <div class="footerNav">
+       <div class="footerNavLeft">
+        <div class="footerCol1">
+          <h3>dc comics</h3>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+
+        </div>
+
+        <div class="footerCol2">
+          col 2
+
+        </div>
+        
+        <div class="footerCol3">
+          col 3
+
+        </div>
+
+       </div>
+
+       <div class="footerNavRight">
+
+       </div>
+
+      </div>
+    </div>
+
+
+  </div>
+</footer>
