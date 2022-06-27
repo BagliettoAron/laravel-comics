73890df304e7387b5023b518a106f8fb28d7914d
@BagliettoAron <aronbaglietto88@gmail.com>
2022-06-27 18:00:58 +0200

    Add:header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,47 @@
 <header>
-    header
-  </header>
+    {{-- top header --}}
+    <div class="topHeader">
+        <div class="container ">
+            
+                <span>dc power visa
+                </span>
+                <span>additional dc sites</span>
+        </div>
+
+    </div>
+
+    {{-- bottom header --}}
+    <div class="bottomHeader">
+        <div class="container">
+            {{-- logo --}}
+            <div class="dcLogo">
+                <img src="{{ asset ('img/dc-logo.png')}}" alt="DC Logo">
+            </div>
+            {{-- nav --}}
+            <div class="navMenu">
+                <ul>
+                    <li>character</li>
+                    <li>comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibles</li>
+                    <li>videos</li>
+                    <li>fans</li>
+                    <li>news</li>
+                    <li>shop</li>
+                </ul>
+            </div>
+            {{-- search --}}
+            <div class="searchBox">
+                <input type="text" placeholder="search">
+            </div>
+        </div>
+    </div>
+
+    {{-- jumbotron --}}
+    <div class="jumbotron">
+        
+      
+    </div>
+</header>
